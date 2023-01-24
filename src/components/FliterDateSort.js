e56c4a670adbf96f41985a1dfde73944aedc9818
praddyyyy <pradeeshxdev@gmail.com>
2023-01-24 09:00:41 +0530

    add functionalities to filter, date and sort

--- src/components/FliterDateSort.js
+++ src/components/FliterDateSort.js
@@ -1,32 +1,78 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
-import IonIcons from 'react-native-vector-icons/Ionicons'
-const FliterDateSort = () => {
+import { StyleSheet, View } from 'react-native'
+import React, { useState } from 'react'
+import Dimensions from '../constants/Dimensions'
+import FilterDateSortButtons from './ClubScreen/FilterDateSortButtons'
+import FilterModalComponent from './FilterModalComponent'
+import DateTimePicker from '@react-native-community/datetimepicker';
+import SortModalComponent from './SortModalComponent'
+
+const FilterDateSort = (props) => {
+    const { type } = props;
+    const [date, setDate] = useState(new Date());
+    const [mode, setMode] = useState('date');
+    const [show, setShow] = useState(false);
+
+
+    const onChange = (event, selectedDate) => {
+        const currentDate = selectedDate || date;
+        setShow(Platform.OS === 'ios');
+        setDate(currentDate);
+
+        let tempDate = new Date(currentDate);
+        let fDate = tempDate.getDate() + '-' + (tempDate.getMonth() + 1) + '-' + tempDate.getFullYear();
+    }
+
+    const showMode = (currentMode) => {
+        setShow(true);
+        setMode(currentMode);
+    }
+
+    const [isFilterVisible, setIsFilterVisible] = useState(false);
+
+    const FilterPressHandler = () => {
+        return (
+            setIsFilterVisible(true)
+        )
+    }
+
+    const CalendarPressHandler = () => {
+        showMode('date')
+    }
+
+    const [isSortVisible, setIsSortVisible] = useState(false);
+
+    const SortPressHandler = () => {
+        return (
+            setIsSortVisible(true)
+        )
+    }
     return (
         <View style={styles.container}>
-            <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center', borderWidth: 1, borderColor: 'white' }}>
-                <Text style={{ fontSize: 18, fontFamily: 'Alata', color: 'white', marginRight: 10, bottom: 3}}>Filter</Text>
-                <MaterialCommunityIcons name='tune' color='white' size={20} />
-            </View>
-            <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center', borderWidth: 1, borderColor: 'white' }}>
-                <Text style={{ fontSize: 18, fontFamily: 'Alata', color: 'white', marginRight: 10, bottom: 3 }}>08 May</Text>
-                <MaterialCommunityIcons name='calendar-month' color='white' size={20} />
-            </View>
-            <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center', borderWidth: 1, borderColor: 'white' }}>
-                <Text style={{ fontSize: 18, fontFamily: 'Alata', color: 'white', marginRight: 10, bottom: 3 }}>Sort</Text>
-                <IonIcons name='swap-vertical' color='white' size={20} />
-            </View>
+            <FilterModalComponent type={type} isFilterVisible={isFilterVisible} setIsFilterVisible={setIsFilterVisible} />
+            <SortModalComponent isSortVisible={isSortVisible} setIsSortVisible={setIsSortVisible} />
+            <FilterDateSortButtons icon='tune' text='Filter' pressHandler={FilterPressHandler} />
+            <FilterDateSortButtons icon='calendar-month' text={date.toDateString().slice(4, 10)} pressHandler={CalendarPressHandler} />
+            <FilterDateSortButtons icon='swap-vertical' text='Sort' pressHandler={SortPressHandler} />
+            {show && (
+                <DateTimePicker
+                    testID='dateTimePicker'
+                    value={date}
+                    mode={mode}
+                    is24Hour={true}
+                    display='default'
+                    onChange={onChange}
+                />
+            )}
         </View>
     )
 }
 
-export default FliterDateSort
+export default FilterDateSort
 
 const styles = StyleSheet.create({
     container: {
         flexDirection: 'row',
-        height: 40,
+        height: Dimensions.SCREEN_HEIGHT * 0.07,
         justifyContent: 'space-around',
         alignItems: 'center'
     }
