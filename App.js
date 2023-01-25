226f216df23b8c35b1cecd105887a321ffc184f9
praddyyyy <pradeeshxdev@gmail.com>
2023-01-25 22:46:45 +0530

    Add Provider for react-redux

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StyleSheet } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import Navigation from './src/navigation';
+import store from './store';
+import { Provider } from 'react-redux'
 
 export default function App() {
-// TODO fix autorotate
+  // TODO fix autorotate
   return (
     <SafeAreaView style={styles.container}>
-      <Navigation />
+      <Provider store={store}>
+        <Navigation />
+      </Provider>
     </SafeAreaView>
 
   );
