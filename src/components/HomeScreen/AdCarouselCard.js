6987523291a94a2acec6b346277c25557b126fa4
Pradeeshwar <pradeeshxdev@gmail.com>
2023-03-05 20:26:32 +0530

    Add border to images

--- src/components/HomeScreen/AdCarouselCard.js
+++ src/components/HomeScreen/AdCarouselCard.js
@@ -28,8 +28,10 @@ const AdCarouselCard = (props) => {
             marginLeft: props.index === 0 ? Dimensions.CAROUSEL_SIDE_CARD_LENGTH : Dimensions.CAROUSEL_SPACING,
             marginRight: props.index === 2 ? Dimensions.CAROUSEL_SIDE_CARD_LENGTH : Dimensions.CAROUSEL_SPACING
         }]}>
+
             <Image source={{ uri: props.source }} style={{ height: '100%', width: '100%' }} />
-        </Animated.View>
+
+        </Animated.View >
     )
 }
 
@@ -40,6 +42,8 @@ const styles = StyleSheet.create({
         width: Dimensions.CAROUSEL_CARD_LENGTH,
         height: Dimensions.SCREEN_HEIGHT * 0.2,
         overflow: 'hidden',
-        borderRadius: 15
+        borderRadius: 15,
+        borderWidth: 0.5,
+        borderColor: 'rgba(255,255,255,0.3)',
     }
 })
