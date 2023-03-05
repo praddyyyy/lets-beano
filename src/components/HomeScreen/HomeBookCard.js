5f858732376563c05a73b1eeb482bceff29a3cf2
Pradeeshwar <pradeeshxdev@gmail.com>
2023-03-05 20:27:20 +0530

    Add text overlay on image

--- src/components/HomeScreen/HomeBookCard.js
+++ src/components/HomeScreen/HomeBookCard.js
@@ -1,10 +1,15 @@
-import { TouchableOpacity, Image } from 'react-native'
+import { TouchableOpacity, Image, View, Text } from 'react-native'
 import React from 'react'
 
 const HomeBookCard = (props) => {
+    const text = props.text
     return (
-        <TouchableOpacity activeOpacity={0.5}>
-            <Image style={[{ borderRadius: 10 }, props.style]} source={props.url} />
+        // TODO Check DJ's text
+        <TouchableOpacity style={{ borderRadius: 10 }} activeOpacity={0.5}>
+            <Image style={[{ borderRadius: 10, opacity: 0.6, borderWidth: 0.5, borderColor: 'rgba(255,255,255,0.3)' }, props.style]} source={props.url} />
+            <View style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center', borderRadius: 10 }}>
+                <Text style={{ color: '#fff', fontSize: 20, fontFamily: 'Montserrat-Bold' }}>{text}</Text>
+            </View>
         </TouchableOpacity>
     )
 }
