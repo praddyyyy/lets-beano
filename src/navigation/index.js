dba718af3475062cebf6bf3a531f8f376929b87b
Pradeeshwar <pradeeshxdev@gmail.com>
2023-07-01 12:35:04 +0530

    add userStack and authStack

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,14 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { useEffect, useState } from 'react'
+import userStack from './userStack'
+import authStack from './authStack'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from '../../firebase-config'
 
 const Navigation = () => {
+  const [currentUser, setCurrentUser] = useState(false)
+
+  const userHandler = (user) => {
+    (user && auth.currentUser.emailVerified) ? setCurrentUser(true) : setCurrentUser(false)
+  }
+
+  useEffect(() =>
+    onAuthStateChanged(auth, (user) => {
+      console.log('Auth Stack Change')
+      // console.log('User Value: ', user)
+      if (user) {
+        console.log('User Boolean: ', user && auth.currentUser.emailVerified)
+        console.log("Is Email Verified? :", auth.currentUser.emailVerified)
+      } else {
+        console.log('User not created yet')
+      }
+      userHandler(user)
+      // console.log("Current User: ", currentUser)
+    })
+    , [])
+  // console.log("Current User: ", currentUser)
   return (
-    <View>
-      <Text>index</Text>
-    </View>
+    currentUser ? userStack() : authStack()
   )
 }
 
 export default Navigation
-
-const styles = StyleSheet.create({})
