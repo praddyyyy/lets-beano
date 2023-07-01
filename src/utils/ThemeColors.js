b20533bcddecc1356c6544c91f5896f99897a4f7
Pradeeshwar <pradeeshxdev@gmail.com>
2023-07-01 12:33:02 +0530

    add new colors

--- src/utils/ThemeColors.js
+++ src/utils/ThemeColors.js
@@ -1,6 +1,14 @@
 export const COLORS = {
-    background: '#202020',
+    // background: '#1b1b1b',
+    background: '#101010',
+    // background: '#000',
     white: '#fff',
     gray: '#C4C4C4',
     primary: '#FF4C68',
+
+    // for FAB
+    accent: '#112233',
+    red: '#FF4C68',
+    lightRed: '#ff4f7e',
+    darkRed: '#d9365e',
 }
