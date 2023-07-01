014f7f87a03aa590669174190f546e8f9c11186d
Pradeeshwar <pradeeshxdev@gmail.com>
2023-07-01 12:35:36 +0530

    change icon size

--- src/components/HomeScreen/TopBar.js
+++ src/components/HomeScreen/TopBar.js
@@ -10,18 +10,18 @@ const TopBar = () => {
     return (
         <View style={styles.container}>
             <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-                <Icon type='ionicon' name='ios-location' color='white' size={moderateScale(20, Dimensions.SCALING_FACTOR)} />
-                <Text style={{ color: 'white', fontFamily: 'Blinker_600SemiBold', fontSize: moderateScale(12, Dimensions.SCALING_FACTOR), marginLeft: 5 }}>Kalavakkam, Chennai</Text>
+                <Icon type='ionicon' name='ios-location' color='white' size={moderateScale(24, Dimensions.SCALING_FACTOR)} />
+                <Text style={{ color: 'white', fontFamily: 'Blinker_600SemiBold', fontSize: moderateScale(15, Dimensions.SCALING_FACTOR), marginLeft: 5 }}>Kalavakkam, Chennai</Text>
             </View>
             <View style={{ flexDirection: 'row' }}>
                 <View style={{ marginRight: moderateScale(14, Dimensions.SCALING_FACTOR) }}>
                     <TouchableOpacity onPress={() => { navigation.navigate('QRScannerScreen') }}>
-                        <Icon type='material-icon' name='qr-code-scanner' color='white' size={moderateScale(20, Dimensions.SCALING_FACTOR)} />
+                        <Icon type='material-icon' name='qr-code-scanner' color='white' size={moderateScale(24, Dimensions.SCALING_FACTOR)} />
                     </TouchableOpacity>
                 </View>
                 <View>
                     <TouchableOpacity onPress={() => { navigation.navigate('ProfileScreen') }}>
-                        <Icon type='font-awesome' name='user-circle-o' color='white' size={moderateScale(20, Dimensions.SCALING_FACTOR)} />
+                        <Icon type='font-awesome' name='user-circle-o' color='white' size={moderateScale(24, Dimensions.SCALING_FACTOR)} />
                     </TouchableOpacity>
                 </View>
             </View>
