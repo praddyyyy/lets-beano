df19a2c1f1547e5f1527c882c593634a29e95671
Pradeeshwar <pradeeshxdev@gmail.com>
2023-08-02 11:46:49 +0530

    change font size

--- src/components/HomeScreen/SubTitleComponent.js
+++ src/components/HomeScreen/SubTitleComponent.js
@@ -8,7 +8,7 @@ const SubTitleComponent = (props) => {
         <View style={styles.container}>
             <Divider style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: '50%' }} width={1.5} />
             <View style={{ alignSelf: 'flex-start', borderWidth: 1, borderColor: '#fff', borderRadius: 15, backgroundColor: '#1f1f1f', marginHorizontal: 15 }}>
-                <Text style={{ color: 'white', fontFamily: 'Montserrat_700Bold', fontSize: 22, marginHorizontal: 10, textAlign: 'center' }}>{text}</Text>
+                <Text style={{ color: 'white', fontFamily: 'Montserrat_700Bold', fontSize: 20, marginHorizontal: 10, textAlign: 'center' }}>{text}</Text>
             </View>
         </View>
     )
