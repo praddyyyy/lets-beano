70047b00ada5fd8753be4b0712ac5391a68cda42
Pradeeshwar <pradeeshxdev@gmail.com>
2023-08-03 16:27:15 +0530

    add new slices

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,13 +1,21 @@
 
 import { combineReducers } from '@reduxjs/toolkit';
-import clubDataReducer from './features/clubDataSlice';
-import clubFilterReducer from './features/clubFilterSlice';
-import clubSortReducer from './features/clubSortSlice';
+import clubDataReducer from './features/clubs/clubDataSlice';
+import clubFilterReducer from './features/clubs/clubFilterSlice';
+import clubSortReducer from './features/clubs/clubSortSlice';
+import homeCarouselReducer from './features/home/homeCarouselSlice';
+import homeOffersSlice from './features/home/homeOffersSlice';
+import homeTrendingSlice from './features/home/homeTrendingSlice';
+import homeExclusiveSlice from './features/home/homeExclusiveSlice';
 
 const rootReducer = combineReducers({
     clubData: clubDataReducer,
     clubFilter: clubFilterReducer,
     clubSort: clubSortReducer,
+    homeCarousel: homeCarouselReducer,
+    homeOffers: homeOffersSlice,
+    homeTrending: homeTrendingSlice,
+    homeExclusive: homeExclusiveSlice
 });
 
 export default rootReducer;
