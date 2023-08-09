e769419f7142038f935e13665c90e68d5de19a9b
Pradeeshwar <pradeeshxdev@gmail.com>
2023-08-09 20:07:54 +0530

    setup locationa nd notification

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback } from 'react'
+import { useEffect, useState, useCallback, useRef } from 'react'
 import userStack from './userStack'
 import authStack from './authStack'
 import { onAuthStateChanged } from 'firebase/auth'
@@ -11,25 +11,81 @@ import { MontserratAlternates_900Black } from '@expo-google-fonts/montserrat-alt
 import { Blinker_300Light, Blinker_400Regular, Blinker_600SemiBold, Blinker_700Bold } from '@expo-google-fonts/blinker'
 import * as SplashScreen from 'expo-splash-screen';
 
-import { Text, View } from 'react-native';
+import { Alert, Text, View, BackHandler } from 'react-native';
 import { StyleSheet } from 'react-native'
 import { COLORS } from '../utils/ThemeColors'
 import Dimensions from '../utils/Dimensions'
 import { moderateScale } from 'react-native-size-matters'
 import { StatusBar } from 'expo-status-bar'
 
-// Keep the splash screen visible while we fetch resources
-SplashScreen.preventAutoHideAsync();
+import * as Location from 'expo-location';
+
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import Constants from "expo-constants";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
+
+async function registerForPushNotificationsAsync() {
+  let token;
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token =
+      await Notifications.getExpoPushTokenAsync({
+        projectId: Constants.expoConfig.extra.eas.projectId,
+      })
+    console.log(token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+  return token;
+}
 
 const Navigation = () => {
+  // Keep the splash screen visible while we fetch resources
+  // SplashScreen.preventAutoHideAsync();
+
   const [initializing, setInitializing] = useState(true)
   const [currentUser, setCurrentUser] = useState(false)
+
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
   const userHandler = (user) => {
     (user && auth.currentUser.emailVerified) ? setCurrentUser(true) : setCurrentUser(false)
   }
 
-  useEffect(() =>
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       console.log('Auth Stack Change')
       // console.log('User Value: ', user)
@@ -42,8 +98,35 @@ const Navigation = () => {
       userHandler(user)
       if (initializing) setInitializing(false)
       // console.log("Current User: ", currentUser)
-    })
-    , [])
+    });
+
+    (async () => {
+
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, [])
   // console.log("Current User: ", currentUser)
 
   // Load any resources or data that we need prior to rendering the app
@@ -64,12 +147,32 @@ const Navigation = () => {
   // Once the fonts are loaded, hide the splash screen
   const onLayoutRootView = useCallback(async () => {
     if (fontsLoaded) {
-      await SplashScreen.hideAsync();
+      // await SplashScreen.hideAsync();
+      // setInitializing(false)
+      console.log('font loaded') //TODO remove & check why splashscreen is not hiding
     }
   }, [fontsLoaded]);
 
   if (!fontsLoaded) {
     return null;
+  }
+
+  if (errorMsg) {
+    Alert.alert(
+      'Location Permission Denied!',
+      errorMsg,
+      [
+        {
+          text: 'Cancel',
+          onPress: () => BackHandler.exitApp(),
+          style: 'cancel',
+        },
+      ],
+      {
+        cancelable: true,
+        onDismiss: () => BackHandler.exitApp(),
+      },
+    );
   }
 
   return (
