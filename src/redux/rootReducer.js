3b1fe137f61b0ecf08b9b51efcadf55ff6af49d3
Pradeeshwar <pradeeshxdev@gmail.com>
2023-08-09 20:07:12 +0530

    add eventDataSlice reduceer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,19 +3,25 @@ import { combineReducers } from '@reduxjs/toolkit';
 import clubDataReducer from './features/clubs/clubDataSlice';
 import clubFilterReducer from './features/clubs/clubFilterSlice';
 import clubSortReducer from './features/clubs/clubSortSlice';
+
 import homeCarouselReducer from './features/home/homeCarouselSlice';
 import homeOffersSlice from './features/home/homeOffersSlice';
 import homeTrendingSlice from './features/home/homeTrendingSlice';
 import homeExclusiveSlice from './features/home/homeExclusiveSlice';
 
+import eventDataSlice from './features/events/eventDataSlice';
+
 const rootReducer = combineReducers({
     clubData: clubDataReducer,
     clubFilter: clubFilterReducer,
     clubSort: clubSortReducer,
+
     homeCarousel: homeCarouselReducer,
     homeOffers: homeOffersSlice,
     homeTrending: homeTrendingSlice,
-    homeExclusive: homeExclusiveSlice
+    homeExclusive: homeExclusiveSlice,
+
+    eventData: eventDataSlice,
 });
 
 export default rootReducer;
