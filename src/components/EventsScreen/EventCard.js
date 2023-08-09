f5837913b5e44b0d9aa3599090471a353f22e28a
Pradeeshwar <pradeeshxdev@gmail.com>
2023-08-09 20:06:04 +0530

    add more data

--- src/components/EventsScreen/EventCard.js
+++ src/components/EventsScreen/EventCard.js
@@ -1,12 +1,25 @@
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
+import moment from 'moment'
 
 const EventCard = (props) => {
-    const { eventName, imageUri, eventAddress, eventPrice } = props
+    const { eventName, imageUri, eventAddress, eventPrice, eventKeywords, end_time, start_time, eventLocation, eventContact, eventOrganizedBy } = props
     const navigation = useNavigation()
+
     const pressHandler = () => {
-        navigation.navigate('EventDetailScreen', { eventName: eventName, imageUri: imageUri, eventAddress: eventAddress, eventPrice: eventPrice })
+        navigation.navigate(
+            'EventDetailScreen',
+            { eventName: eventName, imageUri: imageUri, eventAddress: eventAddress, eventPrice: eventPrice, eventKeywords: eventKeywords, end_time: end_time, start_time: start_time, eventLocation: eventLocation, eventContact: eventContact, eventOrganizedBy: eventOrganizedBy }
+        )
     }
+
+    const convertISODateToCustomFormat = (isoDateString) => {
+        const formattedDate = moment(isoDateString).format("Do MMM");
+        return formattedDate;
+    };
+
+    const formattedDate = convertISODateToCustomFormat(start_time)
+
     return (
         <View style={{ marginVertical: 15, marginRight: 20 }}>
             {/* #TODO change opacity of touchable opacity */}
@@ -15,14 +28,14 @@ const EventCard = (props) => {
                     <Image source={{ uri: imageUri }} style={{ borderTopLeftRadius: 15, borderTopRightRadius: 15, width: '100%', height: '85%' }} />
                     <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', backgroundColor: '#1f1f1f', height: '100%', flex: 1, borderBottomLeftRadius: 15, borderBottomRightRadius: 15 }}>
                         {/* #TODO Get actual date and distance from data */}
-                        <Text style={{ color: '#fff', marginLeft: 10, fontFamily: 'Montserrat_600SemiBold', fontSize: 12 }}>Sun, 20 Nov</Text>
+                        <Text style={{ color: '#fff', marginLeft: 10, fontFamily: 'Montserrat_600SemiBold', fontSize: 12 }}>{formattedDate}</Text>
                         <View style={{ borderRadius: 50, backgroundColor: '#000', marginRight: 10 }}>
                             <Text style={{ color: '#fff', paddingHorizontal: 10, paddingVertical: 5, fontFamily: 'Montserrat_600SemiBold', fontSize: 10 }}>12.3 Km</Text>
                         </View>
                     </View>
                 </View>
             </TouchableOpacity>
-            <TouchableOpacity style={{marginTop: 5}} onPress={pressHandler}>
+            <TouchableOpacity style={{ marginTop: 5 }} onPress={pressHandler}>
                 {/* #TODO maybe insert icons for location and currency */}
                 <Text style={{ color: '#fff', fontFamily: 'Montserrat_600SemiBold', fontSize: 15 }}>{eventName}</Text>
                 <Text style={{ color: '#fff', fontFamily: 'Montserrat_400Regular', paddingVertical: 5, fontSize: 13 }}>{eventAddress}</Text>
