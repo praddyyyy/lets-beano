d27e7c566daf68f076b3fa0b1be03e96bbe2338b
Pradeeshwar <pradeeshxdev@gmail.com>
2023-08-09 20:05:46 +0530

    disable map scroll

--- src/components/EventDetailScreen/VenueTab.js
+++ src/components/EventDetailScreen/VenueTab.js
@@ -9,12 +9,16 @@ const VenueTab = () => {
             {/* TODO Add animation as in https://blog.logrocket.com/react-native-maps-introduction/ */}
             {/* TODO Change marker type */}
             {/* TODO Restrict map maybe */}
-            <MapView style={{ width: '100%', height: '100%' }} initialRegion={{
-                latitude: 37.78825,
-                longitude: -122.4324,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
-            }}>
+            <MapView
+                style={{ width: '100%', height: '100%' }}
+                initialRegion={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+                scrollEnabled={false}
+            >
                 <Marker coordinate={{
                     latitude: 37.78825,
                     longitude: -122.4324,
