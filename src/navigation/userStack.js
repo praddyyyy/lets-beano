f3965360186c6e8a30e3c58ddb1f754c92c176ab
Pradeeshwar <pradeeshxdev@gmail.com>
2023-08-09 20:07:30 +0530

    setup notification and location

--- src/navigation/userStack.js
+++ src/navigation/userStack.js
@@ -8,6 +8,7 @@ import ClubDetailScreen from '../screens/ClubDetailScreen'
 import EventsScreen from '../screens/EventsScreen'
 import EventDetailScreen from '../screens/EventDetailScreen'
 import ProfileScreen from '../screens/ProfileScreen'
+import { Linking } from 'react-native'
 // import SearchScreen from '../screens/SearchScreen'
 // import DummyScreen from '../screens/DummyScreen'
 // import EventBookingScreen from '../screens/EventBookingScreen'
@@ -15,12 +16,58 @@ import ProfileScreen from '../screens/ProfileScreen'
 // import DJScreen from '../screens/DJScreen'
 // import QRScannerScreen from '../screens/QRScannerScreen'
 
+import * as Notifications from 'expo-notifications';
 
 const Stack = createNativeStackNavigator()
 
 const userStack = () => {
+
     return (
-        <NavigationContainer>
+        <NavigationContainer
+            linking={{
+                config: {
+                    // Configuration for linking
+                },
+                prefixes: ['beano://'],
+                async getInitialURL() {
+                    // First, you may want to do the default deep link handling
+                    // Check if app was opened from a deep link
+                    const url = await Linking.getInitialURL();
+
+                    if (url != null) {
+                        return url;
+                    }
+
+                    // Handle URL from expo push notifications
+                    const response = await Notifications.getLastNotificationResponseAsync();
+
+                    return response?.notification.request.content.data.url;
+                },
+                subscribe(listener) {
+                    const onReceiveURL = ({ url }) => listener(url);
+
+                    // Listen to incoming links from deep linking
+                    const eventListenerSubscription = Linking.addEventListener('url', onReceiveURL);
+
+                    // Listen to expo push notifications
+                    const subscription = Notifications.addNotificationResponseReceivedListener(response => {
+                        const url = response.notification.request.content.data.url;
+                        // Any custom logic to see whether the URL needs to be handled
+
+                        // Let React Navigation handle the URL
+                        if (url) {
+                            listener(url);
+                        }
+                    });
+
+                    return () => {
+                        // Clean up the event listeners
+                        eventListenerSubscription.remove();
+                        subscription.remove();
+                    };
+                },
+            }}
+        >
             <Stack.Navigator initialRouteName='HomeScreen' screenOptions={{
                 headerShown: false,
                 gestureEnabled: true,
