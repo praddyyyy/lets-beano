03f1066ad45f33c42c9cfebad34114e0da726797
Pradeeshwar <pradeeshxdev@gmail.com>
2023-08-16 18:58:28 +0530

    add redux

--- src/components/Global/FilterDateSort/FilterSectionButton.js
+++ src/components/Global/FilterDateSort/FilterSectionButton.js
@@ -1,10 +1,39 @@
 import { Text, TouchableOpacity } from 'react-native'
+import { useSelector, useDispatch } from 'react-redux'
+import { addCategoriesTagsFilter, removeCategoriesTagsFilter } from '../../../redux/features/clubs/clubFilterSlice'
+
 
 const FilterSectionButton = (props) => {
+    const dispatch = useDispatch()
+
+    const selectedFilters = useSelector(state => state.clubFilter.categoriesTagsFilter)
+    // console.log(selectedFilters)
+
+    const handleFilterPress = () => {
+        if (selectedFilters.includes(props.text)) {
+            dispatch(removeCategoriesTagsFilter(props.text));
+        } else {
+            dispatch(addCategoriesTagsFilter(props.text));
+        }
+    }
+
     return (
-        <TouchableOpacity style={{ backgroundColor: "#fff", height: 35, justifyContent: 'center', borderRadius: 50, width: '48%' }}>
-            <Text style={{ textAlign: 'center' }}>{props.text}</Text>
+        // <>
+        //     {
+        //         isPressed ?
+        //             <TouchableOpacity activeOpacity={0.8} onPress={handleFilterPress} style={{ backgroundColor: "#ff4c68", height: 35, justifyContent: 'center', borderRadius: 50, width: '48%' }}>
+        //                 <Text style={{ textAlign: 'center', color: '#fff' }}>{props.text}</Text>
+        //             </TouchableOpacity>
+        //             :
+        //             <TouchableOpacity activeOpacity={0.8} onPress={handleFilterPress} style={{ backgroundColor: "#fff", height: 35, justifyContent: 'center', borderRadius: 50, width: '48%' }}>
+        //                 <Text style={{ textAlign: 'center' }}>{props.text}</Text>
+        //             </TouchableOpacity>
+        //     }
+        // </>
+        <TouchableOpacity activeOpacity={0.8} onPress={handleFilterPress} style={{ backgroundColor: selectedFilters.includes(props.text) ? '#ff4c68' : '#fff', height: 35, justifyContent: 'center', borderRadius: 50, width: '48%' }}>
+            <Text style={{ textAlign: 'center', color: selectedFilters.includes(props.text) ? '#fff' : '#000' }}>{props.text}</Text>
         </TouchableOpacity>
+
     )
 }
 
