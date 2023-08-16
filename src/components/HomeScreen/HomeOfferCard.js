e1ecdc03b43ad3c94b2dcc3d7156ca7c572e8ae0
Pradeeshwar <pradeeshxdev@gmail.com>
2023-08-16 18:57:26 +0530

    add offerText

--- src/components/HomeScreen/HomeOfferCard.js
+++ src/components/HomeScreen/HomeOfferCard.js
@@ -1,11 +1,15 @@
-import { StyleSheet, Image, TouchableOpacity } from 'react-native'
+import { StyleSheet, Image, TouchableOpacity, Text } from 'react-native'
 import Dimensions from '../../utils/Dimensions'
+import { View } from 'react-native'
 
 const HomeOfferCard = (props) => {
     return (
         <TouchableOpacity activeOpacity={0.5}>
             <Image style={styles.ImageStyle} source={{ uri: props.url }} />
-        </TouchableOpacity>
+            <View style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center', borderRadius: 10 }}>
+                <Text style={{ color: '#000', fontSize: 18, fontFamily: 'Montserrat_700Bold' }}>{props.offerText}</Text>
+            </View>
+        </TouchableOpacity >
     )
 }
 
@@ -13,9 +17,8 @@ export default HomeOfferCard
 
 const styles = StyleSheet.create({
     ImageStyle: {
-        height: Dimensions.SCREEN_HEIGHT * 0.10,
-        width: Dimensions.SCREEN_WIDTH * 0.25,
+        height: Dimensions.SCREEN_HEIGHT * 0.08,
+        width: Dimensions.SCREEN_WIDTH * 0.28,
         borderRadius: 10,
-        marginHorizontal: 10,
     }
 })
