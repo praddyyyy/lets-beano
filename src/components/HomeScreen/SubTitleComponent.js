7b69372c5d97f8a21d5c122f560e8819d1bb0045
Pradeeshwar <pradeeshxdev@gmail.com>
2023-08-16 18:56:26 +0530

    change style

--- src/components/HomeScreen/SubTitleComponent.js
+++ src/components/HomeScreen/SubTitleComponent.js
@@ -1,14 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import { Divider } from 'react-native-elements'
-import Dimensions from '../../utils/Dimensions'
 
 const SubTitleComponent = (props) => {
     const text = props.text
     return (
         <View style={styles.container}>
-            <Divider style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: '50%' }} width={1.5} />
-            <View style={{ alignSelf: 'flex-start', borderWidth: 1, borderColor: '#fff', borderRadius: 15, backgroundColor: '#1f1f1f', marginHorizontal: 15 }}>
-                <Text style={{ color: 'white', fontFamily: 'Montserrat_700Bold', fontSize: 20, marginHorizontal: 10, textAlign: 'center' }}>{text}</Text>
+            <Divider style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: '50%' }} color='#fff' width={1} />
+            <View style={{ alignSelf: 'flex-start', borderWidth: 1, borderColor: '#fff', borderRadius: 15, backgroundColor: '#000', marginHorizontal: 15, padding: 3 }}>
+                <Text style={{ color: 'white', fontFamily: 'Montserrat_700Bold', fontSize: 18, marginHorizontal: 10, textAlign: 'center' }}>{text}</Text>
             </View>
         </View>
     )
