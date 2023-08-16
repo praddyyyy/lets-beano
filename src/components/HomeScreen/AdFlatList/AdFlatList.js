dc998405d33d31aa8276032e07b16b45c3a21c43
Pradeeshwar <pradeeshxdev@gmail.com>
2023-08-16 18:56:04 +0530

    add autoplay feature

--- src/components/HomeScreen/AdFlatList/AdFlatList.js
+++ src/components/HomeScreen/AdFlatList/AdFlatList.js
@@ -1,7 +1,7 @@
 import { FlatList, Pressable, View } from 'react-native'
 import Animated from 'react-native-reanimated'
 import Dimensions from '../../../utils/Dimensions'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import AdCarouselCard from './AdCarouselCard'
 import Paginator from './Paginator'
@@ -13,6 +13,43 @@ const AdFlatList = (props) => {
     const [data, setData] = useState(props.data)
 
     const [scrollX, setScrollX] = useState(0);
+    const [activeIndex, setActiveIndex] = useState(0)
+
+    const flatListRef = useRef()
+
+    useEffect(() => {
+        let interval = setInterval(() => {
+            if (activeIndex === data.length - 1) {
+                flatListRef.current.scrollToIndex(
+                    {
+                        index: 0,
+                        animated: true
+                    })
+            }
+            else {
+                flatListRef.current.scrollToIndex(
+                    {
+                        index: activeIndex + 1,
+                        animated: true
+                    })
+            }
+        }, 2500);
+        return () => clearInterval(interval);
+    });
+
+    const getItemLayout = (data, index) => (
+        {
+            length: Dimensions.CAROUSEL_CARD_LENGTH,
+            offset: (Dimensions.CAROUSEL_CARD_LENGTH + 2*Dimensions.CAROUSEL_SPACING) * index,
+            index,
+        }
+    )
+
+    const handleScroll = (event) => {
+        const position = event.nativeEvent.contentOffset.x / (Dimensions.CAROUSEL_CARD_LENGTH);
+        setActiveIndex(Math.round(position));
+        setScrollX(event.nativeEvent.contentOffset.x)
+    }
 
     return (
         <View>
@@ -23,7 +60,7 @@ const AdFlatList = (props) => {
                     showsHorizontalScrollIndicator={false}
                     scrollEventThrottle={16}
                     decelerationRate={0.8}
-                    snapToInterval={Dimensions.CAROUSEL_CARD_LENGTH + (Dimensions.CAROUSEL_SPACING * 4.5)}
+                    // snapToInterval={Dimensions.CAROUSEL_CARD_LENGTH + (Dimensions.CAROUSEL_SPACING * 4.5)}
                     disableIntervalMomentum={true}
                     disableScrollViewPanResponder={true}
                     snapToAlignment={'center'}
@@ -37,9 +74,9 @@ const AdFlatList = (props) => {
                         )
                     }}
                     keyExtractor={(item) => item.id}
-                    onScroll={(event) => {
-                        setScrollX(event.nativeEvent.contentOffset.x)
-                    }}
+                    onScroll={handleScroll}
+                    getItemLayout={getItemLayout}
+                    ref={flatListRef}
                 />
             </Animated.View>
             <Paginator data={data} scrollX={scrollX} />
