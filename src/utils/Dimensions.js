06cd8d6080744819e22cb6206b19239ed0baa5c9
Pradeeshwar <pradeeshxdev@gmail.com>
2023-08-16 18:54:12 +0530

    modify carousel card dimensions

--- src/utils/Dimensions.js
+++ src/utils/Dimensions.js
@@ -3,9 +3,9 @@ import { Dimensions, Platform } from "react-native";
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const SCREEN_HEIGHT = Dimensions.get('window').height;
-const CAROUSEL_CARD_LENGTH = SCREEN_WIDTH * 0.8;
-const CAROUSEL_SPACING = SCREEN_WIDTH * 0.02;
-const CAROUSEL_SIDE_CARD_LENGTH = (SCREEN_WIDTH * 0.18) / 2;
+const CAROUSEL_CARD_LENGTH = SCREEN_WIDTH * 0.9;
+const CAROUSEL_SPACING = SCREEN_WIDTH * 0.01;
+const CAROUSEL_SIDE_CARD_LENGTH = (SCREEN_WIDTH * 0.1) / 2;
 
 // Function to check if the device is a tablet
 // function isLargeScreen() {
