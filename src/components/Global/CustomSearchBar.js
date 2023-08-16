010381ca90d8b0784e41b6208e2e4ea019d59d57
Pradeeshwar <pradeeshxdev@gmail.com>
2023-08-16 18:59:23 +0530

    change searchBar package

--- src/components/Global/CustomSearchBar.js
+++ src/components/Global/CustomSearchBar.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { COLORS } from '../../utils/ThemeColors'
-import { SearchBar } from '@rneui/themed'
+// import { SearchBar } from '@rneui/themed'
+import { Searchbar } from 'react-native-paper';
 import { moderateScale } from 'react-native-size-matters'
 import Dimensions from '../../utils/Dimensions'
 
@@ -12,28 +13,34 @@ const CustomSearchBar = (props) => {
         props.handleSearch(value)
     }
     return (
-        <SearchBar
-            containerStyle={{
-                backgroundColor: COLORS.background,
-                borderBottomWidth: 0,
-                borderTopWidth: 0,
-                paddingHorizontal: moderateScale(10, Dimensions.SCALING_FACTOR),
-                paddingVertical: 10,
-            }}
-            inputContainerStyle={{
-                borderRadius: 15,
-                height: 40,
-                width: '85%',
-                alignSelf: 'center',
-                backgroundColor: COLORS.white,
-            }}
+        // <SearchBar
+        //     containerStyle={{
+        //         backgroundColor: COLORS.background,
+        //         borderBottomWidth: 0,
+        //         borderTopWidth: 0,
+        //         paddingHorizontal: moderateScale(10, Dimensions.SCALING_FACTOR),
+        //         paddingVertical: 10,
+        //     }}
+        //     inputContainerStyle={{
+        //         borderRadius: 15,
+        //         height: 40,
+        //         width: '90%',
+        //         alignSelf: 'center',
+        //         backgroundColor: COLORS.white,
+        //     }}
+        //     placeholder={props.placeholder}
+        //     onChangeText={searchHandler}
+        //     value={search}
+        //     lightTheme={false}
+        //     searchIcon={{ color: COLORS.background, size: 24 }}
+        // />
+        <Searchbar
             placeholder={props.placeholder}
             onChangeText={searchHandler}
             value={search}
-            lightTheme={false}
-            searchIcon={{ color: COLORS.background, size: 24 }}
+            style={{ borderRadius: 50, height: 35, width: '80%', alignSelf: 'center', top: 10, backgroundColor: COLORS.white }}
+            inputStyle={{ fontSize: 16, fontFamily: 'Montserrat_400Regular', alignSelf: 'center' }}
         />
-
 
     )
 }
